89beb29f1802fa4d10fa12ff94eec26eb9b0eb69
Kervina <45807615+Kervina@users.noreply.github.com>
2019-05-28 20:35:12 -0400

    added comments

--- src/SingleAbility.java
+++ src/SingleAbility.java
@@ -7,6 +7,13 @@ public class SingleAbility extends Ability {
         setYAOE(0);
     }
 
+    /**
+     * action: This method will target and affect the selected tiles in a certain pattern
+     * @param jointMap: The map that will be affected
+     * @param i: one of the selected coordinates
+     * @param j: the other selected coordinate
+     * @return: it will return a value based on if an action was valid or not, if it was, it will unindicate everything and reset selectedAbility on levelscreen
+     */
     public boolean action(JointMap jointMap, int i, int j){
         if (jointMap.getIndication(i, j) && !jointMap.isEmpty(i, j)) {
             for (int k = j - getYAOE(); k <= j + getYAOE(); k++) {
@@ -45,6 +52,11 @@ public class SingleAbility extends Ability {
             }
         }
     }
+
+    /**
+     * This method will indicate and make tiles targetable for you to click on it with your cursor to enact an ability.
+     * @param jointMap: The tiles on the jointMap array will be modified (some tiles will be indicated, some will become targetable)
+     */
     public void indicateValidTiles(JointMap jointMap){
         int rangeAhead = getEntitySource().getXGrid() + getXRange();
         int rangeBehind = getEntitySource().getXGrid() - getXRange();
--- src/StarAbility.java
+++ src/StarAbility.java
@@ -5,11 +5,18 @@ public class StarAbility extends Ability {
         super (name, xRange, yRange, status, damage, enemyTarget, friendTarget);
     }
 
+    /**
+     * action: This method will target and affect the selected tiles in a certain pattern
+     * @param jointMap: The map that will be affected
+     * @param i: one of the selected coordinates
+     * @param j: the other selected coordinate
+     * @return: it will return a value based on if an action was valid or not, if it was, it will unindicate everything and reset selectedAbility on levelscreen
+     */
     public boolean action(JointMap jointMap, int i, int j){
         if (jointMap.getTargetable(i, j)) {
             for (int k = 0; k < 3; k++){
                 for (int l = 0; l < 6; l++){
-                    //Yeah might need to revamp Single and AOE ability so one can do empty tiles, one cannot do that
+                    //Hits tiles in a star shape around the indicated tile
                     if (Math.abs(k-j) + Math.abs(l-i) <= 1 && jointMap.tileExists(l,k)){
                         if (getFriendTarget() && jointMap.getTileType(l,k) == jointMap.getTileType(getEntitySource().getXGrid(),getEntitySource().getYGrid())) {
                             jointMap.target(l, k, getDamage(), getStatus());
@@ -60,6 +67,10 @@ public class StarAbility extends Ability {
         }
     }
 
+    /**
+     * This method will indicate and make tiles targetable for you to click on it with your cursor to enact an ability.
+     * @param jointMap: The tiles on the jointMap array will be modified (some tiles will be indicated, some will become targetable)
+     */
     public void indicateValidTiles(JointMap jointMap){
         int rangeAhead = getEntitySource().getXGrid() + getXRange();
         int rangeBehind = getEntitySource().getXGrid() - getXRange();
