68578ac03ae50cedf05a1c99d4e27635407cd999
Ethan K <hoofhorse2000@gmail.com>
2019-05-30 11:30:33 -0400

    fixed offset, almost done hh

--- src/MovingMap.java
+++ src/MovingMap.java
@@ -3,10 +3,8 @@ import java.awt.*;
 public class MovingMap extends OverworldMap{
 
     private int tileSize = 100;
-    private int visibleWidth = this.getMap()[0].length * tileSize;
-    private int visibleHeight = this.getMap().length * tileSize;
-    private int mapWidth = 1366 / tileSize;
-    private int mapHeight = 768 / tileSize;
+    private int visibleWidth = 1366;
+    private int visibleHeight = 768;
 
     public MovingMap(String mapPath, String walkabilityKey){
         super(mapPath,walkabilityKey);
@@ -18,8 +16,8 @@ public class MovingMap extends OverworldMap{
         int highestVisible = player.getY() - (visibleHeight / 2);
         int highestTile = highestVisible / tileSize;
 
-        for (int i = 0; i < mapHeight + 1; i++){
-            for (int j = 0; j < mapWidth + 1; j++){
+        for (int i = 0; i < visibleHeight / tileSize + 1; i++){
+            for (int j = 0; j < visibleWidth / tileSize + 1; j++){
                 if ((0 > leftmostTile + j) || (leftmostTile + j >= getMap().length) ||
                         (0 > highestTile + i) || (highestTile + i >= getMap()[0].length)) {
                     g.setColor(Color.BLACK);
