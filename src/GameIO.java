47f54ee577dbe34fd09e7db8156ad69f6bf70197
Ethan K <hoofhorse2000@gmail.com>
2019-06-11 10:49:38 -0400

    item io fixed for real

--- src/GameIO.java
+++ src/GameIO.java
@@ -640,7 +640,7 @@ public class GameIO {
         Item[] items;
 
         int x, y;
-        String name = "";
+        String name;
         String message;
         String[] tokens;
         int counter = 0;
@@ -653,6 +653,7 @@ public class GameIO {
             tokens = lines[i].split(" ");
             x = Integer.parseInt(tokens[0]);
             y = Integer.parseInt(tokens[1]);
+            name = "";
             for (int j = 2; j < tokens.length; ++j) {
                 name = name.concat(" " + tokens[j]);
             }
@@ -679,8 +680,8 @@ public class GameIO {
         String npcText = readFile("shop_items.txt");
         Item[] items;
         int totalItems;
-        String itemName;
         int itemCost;
+        String itemName = "";
         int x, y;
         int startingIndex = 0;
 
@@ -700,8 +701,9 @@ public class GameIO {
             x = Integer.parseInt(tokens[1]);
             y = Integer.parseInt(tokens[2]);
             itemName = "";
-            for (int j = 3; j < tokens.length; ++i) {
-                itemName.concat(" " + tokens[j]);
+            for (int j = 3; j < tokens.length; ++j) {
+                System.out.println(tokens[j]);
+                itemName = itemName.concat(" " + tokens[j]);
             }
             items[i - startingIndex - 2] = new Item(itemName, itemCost, x, y);
         }
