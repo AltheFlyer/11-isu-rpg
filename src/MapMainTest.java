99e66b379b805fa252e29a8ff9190a0e7e2cb102
Ethan K <hoofhorse2000@gmail.com>
2019-06-11 01:46:15 -0400

    cleaned up shop item io but it still don't work

--- src/MapMainTest.java
+++ src/MapMainTest.java
@@ -5,8 +5,8 @@ public class MapMainTest {
 
         //game.setScreen(new DebugScreen(game));
 
-        game.setScreen(new MapScreen(game,"english_room.txt","walkability.txt",
-                "english_room_npcs.txt", "english_room_objects.txt", 400, 400));
+        game.setScreen(new MapScreen(game,"moving_hallway1.txt","walkability.txt",
+                "moving_hallway1_npcs.txt", "moving_hallway1_objects.txt", 400, 400));
 
         //game.setScreen(new MapScreen(game,"physics_room.txt","walkability.txt",
         //        "physics_room_npcs.txt", "physics_room_objects.txt", 400, 400));
