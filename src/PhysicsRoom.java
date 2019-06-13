52e0ae1b0d84e36d7d703f22ffc653784353b36b
Ethan K <hoofhorse2000@gmail.com>
2019-06-13 04:17:51 -0400

    physc and chem battles

--- src/PhysicsRoom.java
+++ src/PhysicsRoom.java
@@ -10,4 +10,30 @@ public class PhysicsRoom extends RoomMap{
     public PhysicsRoom(GameIO fileManager, String mapPath){
         super(fileManager, mapPath);
     }
+
+    /**
+     * [runEvent]
+     * a sequence of events that happens in the room
+     * returns true when all the events have been completed and the battle is ready to load
+     * @param npcs the array of NPCs inhabiting the room
+     * @return boolean true when all the events have been completed and the battle is ready to load
+     */
+    @Override
+    public boolean runEvent(OverworldNPC[] npcs) {
+        if (npcs[1].isTalking()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * [getLevelName]
+     * returns the name of the level that events in this map will lead to
+     * @return String, the name of the level that events in this map will lead to
+     */
+    @Override
+    public String getLevelName() {
+        return "PhysicsA";
+    }
+
 }
